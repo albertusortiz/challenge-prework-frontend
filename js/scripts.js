ecba770f0978ef72708323e56171ea5a85438dbd
Alberto Vargas <albertovargas.developer@gmail.com>
2020-08-15 23:10:40 -0500

    Add files from project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+<script type="text/javascript"> 
+
+	window.onload = function() { 
+		
+		animateprogress("#progreso_1",91);
+		
+	} 	
+	document.querySelector ("#boton_1").addEventListener ("click", function() { 
+		animateprogress("#progreso_1",91);   
+	});
+</script>
